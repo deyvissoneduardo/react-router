10915a32c5b2ed0a6f9a6e103968ee63f450a8b3
DeyvissonEduardo <deyvissoneduardo22@gmail.com>
2023-03-02 17:43:18 -0300

    search

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
+import SeachForm from './components/SeachForm';
 
 import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -16,11 +18,13 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <NavBar />
+        <SeachForm />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/products/:id' element={<Product />} />
           <Route path='/products/:id/info' element={<Info />} />
+          <Route path='/search' element={<Search />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
