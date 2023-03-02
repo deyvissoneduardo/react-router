72a9be7e12487d401dc42f9b953a1435dbcd4e1d
DeyvissonEduardo <deyvissoneduardo22@gmail.com>
2023-03-02 17:08:52 -0300

    load dynamic

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,23 @@
 import { useParams } from 'react-router-dom'
-const Product = () => {
-    const {id} = useParams();
 
+import { useFetch } from '../hooks/useFetch';
+
+const Product = () => {
+    const { id } = useParams();
+    const url = 'http://localhost:3000/products/' + id;
+    const {data: products, loading, error } = useFetch(url)
+    console.log('products :>> ', products);
     return (
         <div>
             <p>Id do produdo {id}</p>
+            {error && <p> Ocorreu um error</p>}
+            {loading && <p>Carregando....</p>}
+            {products && (
+                <div>
+                    <h1>{products.name}</h1>
+                    <p>{products.price}</p>
+                </div>
+            )}
         </div>
     )
 }
